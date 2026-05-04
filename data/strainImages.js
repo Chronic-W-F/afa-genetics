35c80a87bc850bf2f82cb12f5ae683923047c31c
Chronic-W-F <vandergriff4004@comcast.net>
2026-05-04 03:11:24 -0500

    Update strainImages.js

--- data/strainImages.js
+++ data/strainImages.js
@@ -1,5 +1,5 @@
 const strainImages = {
-  "Fruity Tootie": "/images/fruity-tootie.jpeg",
+  "FrooTie Tootie": "/images/frootie-tootie.png",
   "H8TER-AID": "/images/h8ter-aid.jpeg",
   "Iced Out": "/images/iced-out.jpeg",
   "Blue Razz Haze": "/images/blue-razz-haze.jpeg",
