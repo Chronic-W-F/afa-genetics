4bd29cef7c3181d51af1237c7705c51546cd008e
Chronic-W-F <vandergriff4004@comcast.net>
2026-05-03 19:19:49 -0500

    Add Frozen Yeti image path to strainImages

--- data/strainImages.js
+++ data/strainImages.js
@@ -1,6 +1,7 @@
 const strainImages = {
   "Fa-Kawi Sour": "/images/fa-kawi-sour.jpeg",
 "Frostbite": "/images/frostbite.jpeg",
+"Frozen Yeti": "/images/frozen-yeti.jpeg",
 };
 
 export default strainImages;
