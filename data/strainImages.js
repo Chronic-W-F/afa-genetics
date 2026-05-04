7a04532a15d68c6f5468c7a3437b48d63df6840b
Chronic-W-F <vandergriff4004@comcast.net>
2026-05-03 19:28:36 -0500

    Add new strain images to strainImages.js

--- data/strainImages.js
+++ data/strainImages.js
@@ -3,6 +3,10 @@ const strainImages = {
 "Frostbite": "/images/frostbite.jpeg",
 "Frozen Yeti": "/images/frozen-yeti.jpeg",
 "Island Breeze": "/images/island-breeze.jpeg",
+"Glazed Oreoz": "/images/glazed-oreoz.jpeg",
+  "London Frost": "/images/london-frost.jpeg",
+  "Wave Runner": "/images/wave-runner.jpeg",
+  "Gorilla Kush": "/images/gorilla-kush.jpeg",
 };
 
 export default strainImages;
