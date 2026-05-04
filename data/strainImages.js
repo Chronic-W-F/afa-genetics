5bce8c5763dae6f63e82960a3227461e172e05c9
Chronic-W-F <vandergriff4004@comcast.net>
2026-05-03 19:35:18 -0500

    Reorder strain images and add 'Freezer Burnt'

--- data/strainImages.js
+++ data/strainImages.js
@@ -1,9 +1,11 @@
 const strainImages = {
   "Fa-Kawi Sour": "/images/fa-kawi-sour.jpeg",
-"Frostbite": "/images/frostbite.jpeg",
-"Frozen Yeti": "/images/frozen-yeti.jpeg",
-"Island Breeze": "/images/island-breeze.jpeg",
-"Glazed Oreoz": "/images/glazed-oreoz.jpeg",
+  "Frostbite": "/images/frostbite.jpeg",
+  "Frozen Yeti": "/images/frozen-yeti.jpeg",
+  "Freezer Burnt": "/images/freezer-burnt.jpeg",
+
+  "Island Breeze": "/images/island-breeze.jpeg",
+  "Glazed Oreoz": "/images/glazed-oreoz.jpeg",
   "London Frost": "/images/london-frost.jpeg",
   "Wave Runner": "/images/wave-runner.jpeg",
   "Gorilla Kush": "/images/gorilla-kush.jpeg",
