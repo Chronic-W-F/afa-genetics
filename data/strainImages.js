dfc0b07906c3536a7f8462e3332932dac6762e41
Chronic-W-F <vandergriff4004@comcast.net>
2026-05-03 19:25:25 -0500

    Add Island Breeze image to strainImages

--- data/strainImages.js
+++ data/strainImages.js
@@ -2,6 +2,7 @@ const strainImages = {
   "Fa-Kawi Sour": "/images/fa-kawi-sour.jpeg",
 "Frostbite": "/images/frostbite.jpeg",
 "Frozen Yeti": "/images/frozen-yeti.jpeg",
+"Island Breeze": "/images/island-breeze.jpeg",
 };
 
 export default strainImages;
