025d874f451024506f38bc743f6fd7114f8e60ae
Chronic-W-F <vandergriff4004@comcast.net>
2026-05-05 03:03:02 -0500

    Change Blueberry Parfait image format to PNG

--- data/strainImages.js
+++ data/strainImages.js
@@ -1,5 +1,5 @@
 const strainImages = {
-  "Blueberry Parfait": "/images/blueberry-parfait.jpeg",
+  "Blueberry Parfait": "/images/blueberry-parfait.png",
   "Lemon Cherry Haze": "/images/lemon-cherry-haze.jpeg",
   "Monkey Breath F3": "/images/monkey-breath-f3.png",
   "H8TER-AID": "/images/h8ter-aid.jpeg",
